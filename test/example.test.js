419b786a755f8feffea73891a650e2d8df9e562e
Giovana do Nascimento Pena <giovana.np1@gmail.com>
2021-09-10 18:38:30 -0300

    first commit

---
+++ test/example.test.js
@@ -0,0 +1,11 @@
+import {
+    describe,
+    test,
+    expect
+} from '@jest/globals'
+
+describe('#Testando 1,2,3', () => {
+    test('#teste test', () => {
+        expect(true).toBeTruthy()
+    })
+})
